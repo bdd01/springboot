ddd931059e71d910804a2280188a2ca20faf6d52
biandd3 <biandd3@chinaunicom.cn>
2020-09-07 13:56:37 +0800

    初始化项目

---
+++ src/main/java/com/example/action_manage/ActionManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.action_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/action_manage/ActionManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.action_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
